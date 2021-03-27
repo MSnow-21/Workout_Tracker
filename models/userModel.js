3c58f3aaf7638bb5b4a2c36059cc7bd7603bf9cd
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-03-27 15:10:02 -0400

    Adding new fields to the userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -3,9 +3,22 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    date: {
+    day: {
         type: Date,
         default: Date.now,
-    }
-})
+    },
+    exercises:[
+        {
+            type:{
+                type: String,
+            },
+            name:{
+                type: String,
+            },
+            duration: {
+                type:Number,
+            },
+        
+    }]
+});
 
