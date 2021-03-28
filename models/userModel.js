a3a5c3913087b2fb86f525a7e488d48319cbc7be
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-03-28 08:33:10 -0400

    added index.js in models folder to export schemas. Corrected userModel.js.

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const UserSchema = new Schema({
     day: {
         type: Date,
         default: Date.now,
