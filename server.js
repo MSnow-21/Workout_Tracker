dae1a52335be0dafa5497014dab2053aa88796d1
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-03-28 08:08:24 -0400

    Added html get routes to server.js for index, exercise, and stats.

--- server.js
+++ server.js
@@ -17,4 +17,26 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/exercisedb", { useNewUrlParser: true});
 
+app.get("/", function (req,res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
+app.get("/exercise", function(req,res){
+    res.sendFile(path.join(__dirname,"exercise.html"));
+});
+
+app.get("/stats", function(req,res){
+    res.sendFile(path.join(__dirname, "stats.html"));
+});
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT " + PORT);
+});
